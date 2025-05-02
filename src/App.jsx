172381d99833008102babee9907826ae21303130
pulkitgarg12 <pulkitgarg1003@gmail.com>
2025-05-02 16:51:55 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>My react app</h1>
+      <h2>my react implement with CI/CD</h2>
+      <h3>with Github Action</h3>
+    </div>
+  )
+}
+
+export default App
